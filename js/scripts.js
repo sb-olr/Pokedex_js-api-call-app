ccbd9a8745462e2cc8b7ab7ff7049a34c7a13859
Sujit <sb2011ams@gmail.com>
2022-02-28 17:41:17 +0100

    added list of pokemon test objects

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const pokemonList;
+
+pokemonList.append({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']})
+
+pokemonList.append({name: 'Ivysaur', height: 1, type: ['grass', 'poison']})
+
+pokemonList.append({name: 'Venusaur', height: 2, type: ['grass', 'poison']})
