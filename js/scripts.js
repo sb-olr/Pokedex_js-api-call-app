5473fc9c7245077c622ae81d4c45f8759add2808
Sujit <sb2011ams@gmail.com>
2022-03-01 00:28:44 +0100

    added code to display list items; added some style

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ pokemonList.push({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']});
 pokemonList.push({name: 'Ivysaur', height: 1, type: ['grass', 'poison']});
 
 pokemonList.push({name: 'Venusaur', height: 2, type: ['grass', 'poison']});
+
+for (let poke of pokemonList) {
+    document.write('<div class="pokemon">' + poke.name + ' (height: ' + poke.height +')</div>')
+}
