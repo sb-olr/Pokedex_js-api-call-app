3cc7e24eb20df9fcd8ad4e1deaec5b84d60a9460
Sujit <sb2011ams@gmail.com>
2022-02-28 17:57:49 +0100

    added semi-colons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 const pokemonList;
 
-pokemonList.append({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']})
+pokemonList.append({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']});
 
-pokemonList.append({name: 'Ivysaur', height: 1, type: ['grass', 'poison']})
+pokemonList.append({name: 'Ivysaur', height: 1, type: ['grass', 'poison']});
 
-pokemonList.append({name: 'Venusaur', height: 2, type: ['grass', 'poison']})
+pokemonList.append({name: 'Venusaur', height: 2, type: ['grass', 'poison']});
+
+
