c7af57bfcbac5c169a49d80105a5199496fa0df3
Sujit <sb2011ams@gmail.com>
2022-03-03 10:55:43 +0100

    removed global access using IIFE; improved textcolor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
-const pokemonList = [];
+const pokemanRepository = (() => {
+    const pokemonList = [];
 
-pokemonList.push({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']});
+    const add = (pokeman) => {
+        pokemonList.push(pokeman);
+    }
 
-pokemonList.push({name: 'Ivysaur', height: 1, type: ['grass', 'poison']});
+    const getAll = () => {
+        return pokemonList;
+    }
 
-pokemonList.push({name: 'Venusaur', height: 2, type: ['grass', 'poison']});
+    return {
+        add,
+        getAll
+    };
+})();
 
-pokemonList.forEach( poke => {
-    const {name, height} = poke;
+
+pokemanRepository.add({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']});
+pokemanRepository.add({name: 'Ivysaur', height: 1, type: ['grass', 'poison']});
+pokemanRepository.add({name: 'Venusaur', height: 2, type: ['grass', 'poison']});
+
+pokemanRepository.getAll().forEach( pokemon => {
+    const {name, height} = pokemon;
     const extra = height>=7 ? '<br>Ginormous!!!' : '';
     const text = `<div class="pokemon"> ${name} (height: ${height})${extra}</div>`;
     console.debug(text);
