9c51aac28c1bce7bc1b9ade8061cd13f32ce0962
Sujit <sb2011ams@gmail.com>
2022-03-03 11:26:26 +0100

    added validation of items to be added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
+const neededKeys=['name', 'height', 'type'];
+
 const pokemanRepository = (() => {
     const pokemonList = [];
 
-    const add = (pokeman) => {
-        pokemonList.push(pokeman);
+    const add = pokeman => {
+        if (validKeys(pokeman)){
+            pokemonList.push(pokeman);
+        }
     }
 
     const getAll = () => {
@@ -15,15 +19,22 @@ const pokemanRepository = (() => {
     };
 })();
 
+const validKeys = obj => (typeof(obj)=='object') && (neededKeys.every(key => Object.keys(obj).includes(key)))
 
 pokemanRepository.add({name: 'Bulbasaur', height: 7, type: ['grass', 'poison']});
 pokemanRepository.add({name: 'Ivysaur', height: 1, type: ['grass', 'poison']});
 pokemanRepository.add({name: 'Venusaur', height: 2, type: ['grass', 'poison']});
+pokemanRepository.add({test: 'test', name: 'Testasaur', height: 2, type: ['grass', 'poison']});
+pokemanRepository.add(1);
+pokemanRepository.add(true);
+pokemanRepository.add(false);
+pokemanRepository.add('test');
+
 
 pokemanRepository.getAll().forEach( pokemon => {
     const {name, height} = pokemon;
     const extra = height>=7 ? '<br>Ginormous!!!' : '';
     const text = `<div class="pokemon"> ${name} (height: ${height})${extra}</div>`;
     console.debug(text);
-    document.write(text);
+    // document.write(text);
 })
