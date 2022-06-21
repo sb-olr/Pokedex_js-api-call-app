66860422352736718ee5e23dafea75ef5d8e4844
Sujit <sb2011ams@gmail.com>
2022-06-21 22:03:51 +0200

    added a generic modal

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,7 @@ const pokemonRepository = (() => {
     const loadList = () => {
         showLoadingMessage();
         return fetch(apiUrl).then(response => {
-            // timeout is for demo only as the message disappears too fast!
-            setTimeout(hideLoadingMessage, 2000);
+          hideLoadingMessage();
             console.log("fetch done");
           return response.json();
         }).then(json => {
@@ -58,6 +57,67 @@ const pokemonRepository = (() => {
         }).catch(e => console.error(e));
     }
 
+    // function to show modal with pokemon details
+   function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+
+    // Clear preexisting content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+
+    // Close modal when user clicks on 'Close'
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let modalTitle = document.createElement('h1');
+    modalTitle.innerText = title;
+
+    let modalText = document.createElement('p');
+    modalText.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(modalTitle);
+    modal.appendChild(modalText);
+    modalContainer.appendChild(modal);
+
+    // make modal visible
+    modalContainer.classList.add('is-visible');
+
+    // Close modal when user clicks on 'Close'
+    closeButtonElement.addEventListener('click', hideModal);
+
+    // Close modal when modal is open and user clicks outside of the modal
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer){
+        hideModal();
+      }
+    });
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', function () {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  function hideModal(){
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+  // Close modal when modal is open and user clicks 'Escape'
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+      hideModal();
+    }
+  });
+  
     return {
         getAll,
         addListItem,
